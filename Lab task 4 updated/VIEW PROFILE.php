af0420d16083c07c4ef9bed34c50645107ed9786
Abdul Jabbar Rahat <75233461+AJRahat@users.noreply.github.com>
2021-03-17 15:54:50 +0600

    Add files via upload

---
+++ Lab task 4 updated/VIEW PROFILE.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <title>Profile</title>
+  <h1 ><img src="logo.jpg" alt="company" width="200" height="200"></h1>
+</head>
+<body>
+<table>
+	<tr>
+    <td><a href="LOGOUT.php"> Logout </a></td>
+    </tr>
+</table>
+
+	<h2>PROFILE</h2>
+	<img src="login logo.jpg" alt="logo" width="100" height="100"><br><br>
+	<a href="PROFILE PICTURE"> change </a>
+	<p>Name: Rahat</p>
+	<p>Email: abduljabbarrahat1996@gmail.com</p>
+	<p>Gender: Male</p>
+	<p>Date of Birth: 18/12/1996</p><br>
+	<a href="EDIT PROFILE.php"> Edit Profile </a>
+
+
+<table align="center">
+<tr>
+<td><a href="LOGGED IN DASHBOARD.php"> Dashboard </a></td><br><br>
+<td><a href="VIEW PROFILE.php"> View Profile </a></td><br><br>
+<td><a href="EDIT PROFILE.php"> Edit Profile </a></td><br><br>
+<td><a href="CHANGE PROFILE PICTURE.php"> Change Profile Picture </a></td><br><br>
+<td><a href="CHANGE PASSWORD.php"> Change Password </a></td><br><br>
+<td><a href="LOGOUT.php"> Logout </a></td><br><br>
+</tr>
+</table>
+<?php include 'footer.php';?>
+ </body>
+</html>
